1f5f461955ec229d52ac966c329c121feb67b24d
blackpigg <genesis0717@gmail.com>
2017-05-22 17:31:23 +0900

    second commit

--- nose_detection_usingPG.py
+++ nose_detection_usingPG.py
@@ -10,39 +10,38 @@ import numpy as np
 import tensorflow as tf
 import random
 import itertools
-#import dqn
-#from dqn_conv import DQN
-#import dqn_conv
 import PG_conv
 import PG_value
 import collections
-
 from collections import deque
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import gym
+# import dqn
+# from dqn_conv import DQN
+# import dqn_conv
 
 env = gym.make('ND-v0')
 
 input_size = 784
-output_size = env.action_space.n # number of actions
+output_size = env.action_space.n  # number of actions
 
 dis = 0.9
 REPLAY_MEMORY = 1000
 
-#======================================================================================================================
-#define functions: 
-#   replay train: make batch of data, present value of reward    
 
+# Make batch of data and present value of reward
 def replay_train(mainDQN, targetDQN, train_batch):
+    # mainDQN: ....
+    # targetDQN: ....
+    # train_batch: queue contains training information during batch learning. Refer 'for' statement at below.
     x_stack = np.empty(0).reshape(0, 784)
     h_stack = np.empty(0).reshape(0, 50)
     y_stack = np.empty(0).reshape(0, 1)
     
-    #Get stored information from the buffer
+    # Get stored information from the buffer
     for state, action, reward, next_state, done, history_buffer in train_batch:
-
-        if done ==1:
+        if done == 1:
             Q = reward
         else:
             Q = reward + dis*np.mean(targetDQN.predict(next_state, history_buffer))
@@ -52,7 +51,8 @@ def replay_train(mainDQN, targetDQN, train_batch):
         h_stack = np.vstack([h_stack, np.reshape(history_buffer, 50)])
     return mainDQN.update(x_stack, y_stack, h_stack)
 
-# convert scalar value to one-hot encoded vector
+
+# Convert scalar value to one-hot encoded vector
 def convertToOneHot(vector, num_classes=None):
     assert isinstance(vector, np.ndarray)
     assert len(vector) > 0
@@ -67,15 +67,16 @@ def convertToOneHot(vector, num_classes=None):
     result[np.arange(len(vector)), vector] = 1
     return result.astype(int)
 
+
 # 
 def bot_play(i, mainDQN, success):
-    #See our trained network in action
-    state, state_pt,_ = env.reset()
+    # See our trained network in action
+    state, state_pt, _ = env.reset()
     reward_sum = 0
     done = 0
     step_count = 0
     while not done:
-       # env.render()
+        # env.render()
         a = np.argmax(mainDQN.predict(state))
         next_state, action, reward, done = env.step(a)
         reward_sum += reward
@@ -88,11 +89,11 @@ def bot_play(i, mainDQN, success):
 #            print("episode:{}, steps:{}, reward:{}, success_rate:{}".format(i, step_count, reward, success/10))
             
             break
-        elif 1 == done and reward < 1 :
-#            print("episode:{}, steps:{}, reward:{}, success_rate:{}".format(i, step_count, reward, success/10))
+        elif 1 == done and reward < 1:
+            # print("episode:{}, steps:{}, reward:{}, success_rate:{}".format(i, step_count, reward, success/10))
             break
         elif step_count>2000:
-#            print("episode:{}, steps:{}, reward:{}, success_rate:{}".format(i, step_count, reward, success/10))
+            # print("episode:{}, steps:{}, reward:{}, success_rate:{}".format(i, step_count, reward, success/10))
             break
     return step_count, reward, success
 
@@ -101,45 +102,45 @@ def _get_distance(box1, box2):
         dis = np.sqrt((box1[0]-box2[0])**2+(box1[1]-box2[1])**2)
         return dis
       
- #======================================================================================================================       
+# ======================================================================================================================
 def main():
     weight = np.load('/home/wd/Downloads/downloads/weight_170414.npy')
-    bias   = np.load('/home/wd/Downloads/downloads/bias_170414.npy')
+    bias = np.load('/home/wd/Downloads/downloads/bias_170414.npy')
 #    w3 = np.load('/home/wd/Workspace/RL/dqn_w1.npy')
 #    w4 = np.load('/home/wd/Workspace/RL/dqn_w2.npy')
     cnn_eye = np.load('/home/wd/Workspace/RL/eye_cnnweight.npy')
-    w3  = np.load('/home/wd/Workspace/RL/eye_weight3.npy')
-    w4  = np.load('/home/wd/Workspace/RL/eye_weight4.npy')
+    w3 = np.load('/home/wd/Workspace/RL/eye_weight3.npy')
+    w4 = np.load('/home/wd/Workspace/RL/eye_weight4.npy')
 
     max_episodes = 10000
 
     f = open('/home/wd/Workspace/RL/print.txt','w')
     with tf.Session() as sess:
         
-        #define policy network and value network
-        mainDQN   = PG_conv.DQN(sess, weight, w3, w4, cnn_eye, bias, input_size, output_size, 'main') #
-        value_estimator = PG_value.DQN(sess, weight, w3, w4, cnn_eye, bias, input_size, output_size, "target")#
+        # define policy network and value network
+        mainDQN = PG_conv.DQN(sess, weight, w3, w4, cnn_eye, bias, input_size, output_size, 'main')
+        value_estimator = PG_value.DQN(sess, weight, w3, w4, cnn_eye, bias, input_size, output_size, "target")
 
         tf.global_variables_initializer().run()
         
         for episode in range(max_episodes):
             done = 0
             step_count = 0
-            state, state_pt , gt, whole_image = env.reset()
+            state, state_pt, gt, whole_image = env.reset()
             replay_buffer = []
             tr = collections.namedtuple("tr", ["state", "action", "reward", "next_state", "done", "history_buffer"])
             history_buffer = deque()
             for i in range(10):
-                history_buffer.append([0,0,0,0,0])
+                history_buffer.append([0, 0, 0, 0, 0])
             
             for t in itertools.count():
                 filename = episode
                 plt.imshow(np.reshape(whole_image,(180,180)))
                 ax = plt.gca()
-                plt.plot(gt[0],gt[1],'r+')
+                plt.plot(gt[0], gt[1], 'r+')
                 action_pr = mainDQN.predict(state, history_buffer)
 #                print(action_pr)
-                tmp_action = np.random.choice(np.arange(len(action_pr)), p = action_pr)
+                tmp_action = np.random.choice(np.arange(len(action_pr)), p=action_pr)
 #                print(tmp_action)
                 action = convertToOneHot(np.array([tmp_action]),5)
                 next_state, reward, done, new_state_pt = env.step(action[0])
@@ -149,10 +150,10 @@ def main():
                 if 1 == done and reward == 1:
                     plt.plot(state_pt[0],state_pt[1], 'b+')
                     plt.plot(new_state_pt[0],new_state_pt[1], 'ro')
-                    rect  = patches.Rectangle((new_state_pt[0]-13.5,new_state_pt[1]-13.5), 28, 28, \
-                                              linewidth = 1, edgecolor = 'b', facecolor = 'none')
-                    rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56, \
-                                              linewidth = 1, edgecolor = 'r', facecolor = 'none')
+                    rect = patches.Rectangle((new_state_pt[0]-13.5,new_state_pt[1]-13.5), 28, 28,
+                                              linewidth=1, edgecolor='b', facecolor='none')
+                    rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56,
+                                              linewidth=1, edgecolor='r', facecolor='none')
                     ax.add_patch(rect)
                     ax.add_patch(rect2)
                     plt.savefig(str(filename)+'.png')
@@ -162,13 +163,13 @@ def main():
                     break
                 
                 elif 1 == done and reward == -1: 
-                    if episode % 100 == 0 :
+                    if episode % 100 == 0:
                         plt.plot(state_pt[0],state_pt[1], 'b+')
-                        plt.plot(new_state_pt[0],new_state_pt[1], 'bo')
-                        rect  = patches.Rectangle((new_state_pt[0]-13.5,new_state_pt[1]-13.5), 28, 28, \
-                                                  linewidth = 1, edgecolor = 'b', facecolor = 'none')
-                        rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56, \
-                                                  linewidth = 1, edgecolor = 'r', facecolor = 'none')                        
+                        plt.plot(new_state_pt[0], new_state_pt[1], 'bo')
+                        rect = patches.Rectangle((new_state_pt[0]-13.5,new_state_pt[1]-13.5), 28, 28,
+                                                 linewidth=1, edgecolor='b', facecolor='none')
+                        rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56,
+                                                  linewidth=1, edgecolor='r', facecolor='none')
                         ax.add_patch(rect)
                         ax.add_patch(rect2)
                         plt.savefig(str(filename)+'.png')
@@ -177,14 +178,14 @@ def main():
                     else:
                         plt.close()
                     break
-                elif step_count>50:
+                elif step_count > 50:
                     reward = -1 
-                    plt.plot(state_pt[0],state_pt[1], 'b+')
-                    plt.plot(new_state_pt[0],new_state_pt[1], 'bo')
-                    rect  = patches.Rectangle((new_state_pt[0]-13.5,new_state_pt[1]-13.5), 28, 28, \
-                                              linewidth = 1, edgecolor = 'b', facecolor = 'none')
-                    rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56, \
-                                              linewidth = 1, edgecolor = 'r', facecolor = 'none')                        
+                    plt.plot(state_pt[0], state_pt[1], 'b+')
+                    plt.plot(new_state_pt[0], new_state_pt[1], 'bo')
+                    rect = patches.Rectangle((new_state_pt[0]-13.5, new_state_pt[1]-13.5), 28, 28,
+                                             linewidth=1, edgecolor='b', facecolor='none')
+                    rect2 = patches.Rectangle((gt[0]-28, gt[1]-28), 56, 56,
+                                              linewidth=1, edgecolor='r', facecolor='none')
                     ax.add_patch(rect)
                     ax.add_patch(rect2)
                     plt.savefig(str(filename)+'.png')
@@ -193,24 +194,16 @@ def main():
                     break
                 history_buffer.popleft()
                 history_buffer.append(action[0])
-                replay_buffer.append(tr(state, action, reward, next_state, done,\
-                                       history_buffer))
+                replay_buffer.append(tr(state, action, reward, next_state, done, history_buffer))
                 state = next_state
                 step_count += 1
-                
-                
-                
-            for t, tr in enumerate(replay_buffer):
 
+            for t, tr in enumerate(replay_buffer):
                 total_return = sum(dis**i * t.reward for i, t in enumerate(replay_buffer[t:]))
-                
-                value_estimator.update(tr.state, total_return, np.reshape(tr.history_buffer, (1,50)))
-                
-                baseline = value_estimator.predict(tr.state, np.reshape(tr.history_buffer, (1,50)))
-                
+                value_estimator.update(tr.state, total_return, np.reshape(tr.history_buffer, (1, 50)))
+                baseline = value_estimator.predict(tr.state, np.reshape(tr.history_buffer, (1, 50)))
                 delta = total_return-baseline
-                
-                mainDQN.update(tr.state, delta, np.reshape(tr.history_buffer, (1,50)), tr.action)
+                mainDQN.update(tr.state, delta, np.reshape(tr.history_buffer, (1, 50)), tr.action)
 
 if __name__ == "__main__":
     main()
